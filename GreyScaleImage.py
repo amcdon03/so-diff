655f697cee4bec8ce3c283077a3c5f56b44dce55
MsMcD <35904765+amcdon03@users.noreply.github.com>
2018-04-07 20:06:56 +0100

    Minor modification, ie spacings

--- GreyScaleImage.py
+++ GreyScaleImage.py
@@ -4,39 +4,39 @@ from BinaryImage import BinaryImage
 class GreyScaleImage(GUIconnect):
 
     def __init__(self, filename):
-        self._data = []
-        self.new_data = []
+        self._values = []
+        self._newList = []
 
         with open(filename, "r") as inFile:
-            values = inFile.readlines()  # Process here?
-            if values[0].strip() != "GreyscaleImage":
+            lines = inFile.readlines()
+            if lines[0].strip() != "GreyscaleImage":
                 raise NotImplementedError("invalid input; image is not grey scale")
 
-            for line in values[1:]:
-                new_line = line.split(",")
-                for each_item in new_line:
-                    self._data.append(int(each_item))
+            for line in lines[1:]:
+                itemList = line.split(",")
+                for each_item in itemList:
+                    self._values.append(int(each_item))
         
 
     def _determineColorValue(self,v):
             return ("#%02x%02x%02x" % (v, v, v))
 
-    def getThreshold(self):
-        for i in range(0, len(self.data), 3):
-            self.new_data.append(self.data[i: i + 3])
-            for v in self.new_data[_,_,v]:
-                t = sum(self.new_data[v]/len(self.new_data))
-        return t
+    def _getThreshold(self):
+        for i in range(0, len(self._values), 3):
+            self._newList.append(self._values[i: i+3])
+            for v in self._newList[_, _, v]:
+                threshold = sum(self._newList[v]/len(self._newList))
+        return threshold
 
-    def binariseImage(self, threshold):
+    def _binariseImage(self, threshold):
         pass
         # Your "Process" Button should call this method.
         # the "threshold" argument is the value extracted from the entry box in your GUI.
         # This method must return an object of type BinaryImage
         # return ???
 
-    def dataForDisplay(self):
-        for x, y, v in self.new_data:
+    def _dataForDisplay(self):
+        for x, y, v in self.values:
             return x, y, self._determineColorValue(v)
 
         # This method returns the data in a form that can be displayed.
